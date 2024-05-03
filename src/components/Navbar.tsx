3abecb65714ca4a00e740e778639a13129dc4735
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-05-03 17:14:40 +0200

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
 
 type Props = {}
 
@@ -8,9 +11,18 @@ export default function Navbar({}: Props) {
             <div className="h-[80px]     w-full    flex   justify-between items-center  max-w-7xl px-3 mx-auto">
                 <p className="flex items-center justify-center gap-2  ">
                     <h2 className="text-gray-500 text-3xl">Weather</h2>
+                    <MdWbSunny className="text-3xl mt-1 text-yellow-300"/>
                 </p>
+                {/*  */}
+                <section className="flex gap-2 items-center">
+                <MdMyLocation 
+                className="text-2xl  text-gray-400 hover:opacity-80 cursor-pointer"
+                />
+                <MdOutlineLocationOn className="text-3xl" />
+                <p className="text-slate-900/80 text-sm"> South Africa </p>
+                <div>{/* SearchBox */}</div>
+                </section>
             </div>
-            Navbar
         </nav>
     );
 }
