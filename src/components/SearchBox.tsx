78e4f60d778fcd9f0617e07049ce6c4ce4a48a07
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-05-06 12:04:08 +0200

    update

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IoSearch } from "react-icons/io5";
+import { cn } from '@/utils/cn';
 
 type Props = {
     className?: string;
@@ -10,7 +11,11 @@ type Props = {
 
 export default function SearchBox(props: Props) {
     return (
-        <form onSubmit={props.onSubmit} className="flex relative items-center justify-center h-10">
+        <form onSubmit={props.onSubmit} 
+        className={cn(
+            "flex relative items-center justify-center h-10",
+            props.className
+        )}>
             <input 
             type="text"
             onChange={props.onChange}
