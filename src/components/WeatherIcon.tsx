9232ffe9f18330bb52e72d34a373deec1772c57d
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-05-07 13:12:15 +0200

    update

--- src/components/WeatherIcon.tsx
+++ src/components/WeatherIcon.tsx
@@ -1,9 +1,21 @@
+/** @format */
+
+import Image from 'next/image';
 import React from 'react';
+import { cn } from '@/utils/cn';
 
 type Props = {}
 
-export default function WeatherIcon({}: Props) {
+export default function WeatherIcon(props: React.HTMLProps<HTMLDivElement> & {iconName: string}) {
     return(
-        <div>WeatherIcon</div>
+        <div  title={props.iconName} {...props} className={cn("relative h-20 w-20")}>
+            <Image
+            width={100}
+            height={100}
+            alt='weather-icon'
+            className='absolute h-full w-full'
+            src={`https://openweathermap.org/img/wn/${props.iconName}@4x.png`} 
+            />
+        </div>
     );
 }
